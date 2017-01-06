182b4c3039975146f8bce865e9ab704bbe19d3b2
鈴木貴広 <takahiro.suzuki@gmail.com>
2017-01-06 19:03:58 +0900

    test

---
+++ Instagram/PostData.swift
@@ -0,0 +1,50 @@
+//
+//  PostData.swift
+//  Instagram
+//
+//  Created by Takahiro Suzuki on 2017/01/05.
+//  Copyright © 2017年 Takahiro Suzuki. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class PostData: NSObject {
+    var id: String?
+    var image: UIImage?
+    var imageString: String?
+    var name: String?
+    var caption: String?
+    var date: NSDate?
+    var likes: [String] = []
+    var isLiked: Bool = false
+    
+    init(snapshot: FIRDataSnapshot, myId: String) {
+        self.id = snapshot.key
+        
+        let valueDictionary = snapshot.value as! [String: Any]
+        
+        imageString = valueDictionary["ïmage"] as? String
+        image = UIImage(data: NSData(base64Encoded: imageString!, options: .ignoreUnknownCharacters)! as Data)
+        
+        self.name = valueDictionary["name"] as? String
+        
+        self.caption = valueDictionary["caption"] as? String
+        
+        
+        let time = valueDictionary["time"] as? String
+        self.date = NSDate(timeIntervalSinceReferenceDate: TimeInterval(time!)!)
+        
+        if let likes = valueDictionary["likes"] as? [String] {
+            self.likes = likes
+        }
+        
+        for likeId in self.likes {
+            if likeId == myId {
+                self.isLiked = true
+                break
+            }
+        }
+    }
+}
